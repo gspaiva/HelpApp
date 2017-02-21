536d3d69782bede56a4599c0e4771fe7d41fd559
gspaiva <gabriel_silvapaiva@hotmail.com>
2017-02-21 00:11:26 -0300

    Login e cadastro funcionando com firebase

--- app/src/main/java/dcc/ufla/br/helpapp/CadastrarUsuarioActivity.java
+++ app/src/main/java/dcc/ufla/br/helpapp/CadastrarUsuarioActivity.java
@@ -5,13 +5,17 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,13 +25,14 @@ import dcc.ufla.br.helpapp.models.User;
 public class CadastrarUsuarioActivity extends AppCompatActivity{
 
 
-    EditText edtNome;
-    EditText edtEmail;
-    EditText edtSenha;
-    EditText edtRepetirSenha;
-    EditText edtEndereco;
-    Button btnConfirmarCadastro;
+    private EditText edtNome;
+    private EditText edtEmail;
+    private EditText edtSenha;
+    private EditText edtRepetirSenha;
+    private EditText edtEndereco;
+    private Button btnConfirmarCadastro;
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    FirebaseAuth auth;
 
 
     @Override
@@ -36,6 +41,9 @@ public class CadastrarUsuarioActivity extends AppCompatActivity{
         setContentView(R.layout.activity_cadastrar_usuario);
 
         getSupportActionBar().setTitle("Cadastro de usuário");
+
+        //get instance
+        auth  = FirebaseAuth.getInstance();
 
         //Get UI elements
         edtNome = (EditText)findViewById(R.id.edtNome);
@@ -51,19 +59,35 @@ public class CadastrarUsuarioActivity extends AppCompatActivity{
 
                 //validar campos antes de mandar
 
-
                 User user = new User(edtNome.getText().toString(),edtEmail.getText().toString(),edtSenha.getText().toString()
                 ,edtEndereco.getText().toString());
 
+                auth.createUserWithEmailAndPassword(user.email,user.senha).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.v("TAG",e.getMessage());
+                    }
+                }).addOnCompleteListener(CadastrarUsuarioActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.v("TAG","QUE TA CONTENCENDO00");
+                                Toast.makeText(CadastrarUsuarioActivity.this,"Dados salvos com sucesso" + String.valueOf(task.isSuccessful()),Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+                /*
+                //passa os dados pro banco de dados
                 DatabaseReference usersRef = mRootRef.child("users");
                 usersRef.push().setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-
-                        Toast.makeText(getBaseContext(),"Dados salvos com sucesso",Toast.LENGTH_SHORT).show();
-
+                        Toast.makeText(getBaseContext(),"Dados salvos com sucesso" + String.valueOf(task.isSuccessful()),Toast.LENGTH_SHORT).show();
                     }
-                });
+
+                });*/
+
+
             }
         });
 
--- app/src/main/java/dcc/ufla/br/helpapp/LoginActivity.java
+++ app/src/main/java/dcc/ufla/br/helpapp/LoginActivity.java
@@ -1,12 +1,26 @@
 package dcc.ufla.br.helpapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,7 +28,9 @@ public class LoginActivity extends AppCompatActivity {
     EditText edtSenha;
     Button btnEntrarLogin;
     TextView txtCadastrarLogin;
-
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
+    public static final  Pattern ValidEmail = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +38,75 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().setTitle("Help - Login");
-
+        //get instance
+        auth = FirebaseAuth.getInstance();
         //get Ui Elements
         edtEmail = (EditText)findViewById(R.id.edtEmailLogin);
         edtSenha = (EditText)findViewById(R.id.edtSenhaLogin);
         btnEntrarLogin =(Button)findViewById(R.id.btnEntrarLogin);
         txtCadastrarLogin = (TextView)findViewById(R.id.txtCadastrarLogin);
+        progressDialog = new ProgressDialog(LoginActivity.this);
 
         txtCadastrarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(),CadastrarUsuarioActivity.class);
-                startActivity(i);
+                startActivity(new Intent(getBaseContext(), CadastrarUsuarioActivity.class));
             }
         });
+
+
+        final Matcher matcher;
+
+        btnEntrarLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!validEmail(edtEmail.getText().toString())){
+                    edtEmail.setError("Seu email está inválido: exemplo@exemplo.com é válido");
+                    return;
+                }
+
+                if(!validSenha(edtSenha.getText().toString())){
+                    edtSenha.setError("Sua senha deve ter no mínimo 6 caracteres ...");
+                    return;
+                }
+
+                progressDialog.setMessage("Logando");
+                progressDialog.show();
+
+                auth.signInWithEmailAndPassword(edtEmail.getText().toString(),edtSenha.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            progressDialog.dismiss();
+
+
+
+                            Toast.makeText(LoginActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+
+                        }
+                        else{
+                            //Intent i = new Intent (LoginActivity.this, DashActivity.class);
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this,"Logado com sucesso" ,Toast.LENGTH_SHORT).show();
+                            //startActivity(
+                        }
+                    }
+                });
+            }
+        });
+
     }
+    public static boolean validEmail(String email){
+        Matcher matcher  = ValidEmail.matcher(email);
+        return matcher.find();
+    }
+    public static boolean validSenha(String senha){
+        if(senha.length() > 5)
+            return true;
+
+        return false;
+    }
+
+
 }
